a8d40f5bb503400aceeacb0b57a7d3ea68ef48c8
Julio Santacruz <julio.santacruzh@gmail.com>
2024-04-04 15:54:08 -0700

    vite config test

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import * as path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/preconst-app/",
+  ...(process.env.NODE_ENV === "development"
+    ? {
+        define: {
+          global: {},
+        },
+      }
+    : {}),
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") ,
+
+        ...(process.env.NODE_ENV !== "development"
+          ? {
+              "./runtimeConfig": "./runtimeConfig.browser", //fix production build
+            }
+          : {}),
+      },
+    ],
   },
-})
+});
