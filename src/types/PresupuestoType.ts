df04a99600d2e1a81ee4322e2d9cdfc2a29434fe
Julio Santacruz <julio.santacruzh@gmail.com>
2024-04-04 16:06:16 -0700

    se removio pnpm-lock.json

---
+++ src/types/PresupuestoType.ts
@@ -0,0 +1,35 @@
+
+export interface FactorSobreCosto{
+  costoIndirecto:number,
+  costoOperativo:number,
+  financiamiento:number,
+  utilidad:number,
+  iva:number,
+  isr:number,
+}
+
+export interface ListadoConcepto{
+  fechaCreacion:string
+  conceptoId:string,
+  cantidad?:number,
+}
+export interface Partida {
+  id: string;
+  clave: string;
+  nombre: string;
+  montoPartida?: number;
+  listadoConceptos?: ListadoConcepto[];
+}
+export interface Presupuesto {
+  id: string;
+  fechaCreacion: string;
+  nombreProyecto: string;
+  descripcionProyecto: string;
+  domicilioProyecto: string;
+  clienteProyecto: string;
+  partidas: Partida[];
+  montoTotal: number;
+  fsc:FactorSobreCosto;
+}
+
+
