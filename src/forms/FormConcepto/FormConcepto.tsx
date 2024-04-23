a2babc771ffe1b37ff946e63b9f9e589a0f7d3d0
Julio Santacruz <julio.santacruzh@gmail.com>
2024-04-22 22:29:50 -0700

    se agrego archivo FormConcepto

---
+++ src/forms/FormConcepto/FormConcepto.tsx
@@ -0,0 +1,90 @@
+import CancelIcon from "@/assets/icons/navbar/CancelIcon";
+import CheckIcon from "@/assets/icons/navbar/CheckIcon";
+import CloseIcon from "@/assets/icons/navbar/CloseIcon";
+import React from "react";
+import "./FormConcepto.scss";
+
+export default function FormConcepto() {
+  return (
+    <div className="newInsumo">
+      <div className="form_header">
+        <h4>Informacion de Concepto</h4>
+        <button>
+          <CloseIcon />
+        </button>
+      </div>
+      <div className="form_content">
+        <form action="" className="newProjectForm">
+          <div className="ConceptoData">
+            <div className="textInput clave">
+              <label className="leLabel" htmlFor="">
+                Clave
+              </label>
+              <input className="leInput" type="text" />
+            </div>
+            <div className="textInput descripcion">
+              <label className="leLabel" htmlFor="">
+                Descripcion
+              </label>
+              <input className="leInput" type="text" />
+            </div>
+            <div className="textInput unidad">
+              <label className="leLabel" htmlFor="">
+                Unidad
+              </label>
+              <input className="leInput" type="text" />
+            </div>
+            <div className="textInput precio">
+              <label className="leLabel" htmlFor="">
+                Precio
+              </label>
+              <input className="leInput" type="text" />
+            </div>
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <td>CLAVE</td>
+                <td>DESCRIPCION</td>
+                <td>UNIDAD</td>
+                <td>CANTIDAD</td>
+                <td>PRECIO</td>
+                <td>ACCIONES</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>MT.0001</td>
+                <td>MATERIAL 1</td>
+                <td>M3</td>
+                <td>2.12</td>
+                <td>12,321.23</td>
+                <td>EDT | DEL</td>
+              </tr>
+              <tr>
+                <td>MT.0002</td>
+                <td>MATERIAL 2</td>
+                <td>M3</td>
+                <td>2.12</td>
+                <td>12,321.23</td>
+                <td>EDT | DEL</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div className="btn-group">
+            <button className="btnForm btnCancel">
+              {" "}
+              <CancelIcon className="btnIcon" /> Cancelar
+            </button>
+            <button className="btnForm btnSave">
+              {" "}
+              <CheckIcon className="btnIcon" /> Guardar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/pages/ListadoInsumos/index.tsx
+++ src/pages/ListadoInsumos/index.tsx
@@ -2,13 +2,14 @@
 import React from "react";
 import "./ListadoInsumos.scss";
 // import FormPartida from "@/forms/FormPartida/FormPartida";
-import FormInsumo from "@/forms/FormInsumo/FormInsumo";
+// import FormInsumo from "@/forms/FormInsumo/FormInsumo";
+import FormConcepto from "@/forms/FormConcepto/FormConcepto";
 
 export default function ListadoInsumos() {
   return (
     <section className="ListadoInsumos">
       <div className="formTest">
-        <FormInsumo/>
+        <FormConcepto/>
       </div>
     </section>
   );
